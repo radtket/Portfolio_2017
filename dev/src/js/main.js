eaa15d42eb0e580726f357e998bc8353c8fa209e
Taylor Radtke <tradtke@aapa.org>
2017-08-02 16:09:17 -0400

    svg icon consistency

--- dev/src/js/main.js
+++ dev/src/js/main.js
@@ -94,3 +94,33 @@ function init_sliders() {
     ]
   });
 }
+
+
+
+
+
+
+/* ---------------------------------------------
+ Contact form
+ --------------------------------------------- */
+ var $contactForm = $('#js-contact-form');
+ $contactForm.submit(function(e) {
+ 	e.preventDefault();
+ 	$.ajax({
+ 		url: '//formspree.io/taylorradtke@gmail.com',
+ 		method: 'POST',
+ 		data: $(this).serialize(),
+ 		dataType: 'json',
+ 		beforeSend: function() {
+ 			$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+ 		},
+ 		success: function(data) {
+ 			$contactForm.find('.alert--loading').hide();
+ 			$contactForm.append('<div class="alert alert--success">Message sent!</div>');
+ 		},
+ 		error: function(err) {
+ 			$contactForm.find('.alert--loading').hide();
+ 			$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+ 		}
+ 	});
+ });
