eb4c40cbfa6d1e63a52adacda7e005c55805c112
Taylor Radtke <tradtke@aapa.org>
2017-08-02 18:15:36 -0400

    smooth scroll added

--- dev/src/js/main.js
+++ dev/src/js/main.js
@@ -22,6 +22,7 @@ $(function() {
   $(window).trigger('resize');
   init_nav_menu();
   init_sliders();
+  init_smooth_scroll();
 });
 
 $(window).on('resize', function() {
@@ -52,6 +53,38 @@ function init_nav_menu() {
 
 
 
+
+function init_smooth_scroll() {
+  $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  	.not('[href="#"]').not('[href="#0"]').click(function(event) {
+  	// On-page links
+  	if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+  		// Figure out element to scroll to
+  		var target = $(this.hash);
+  		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+  		// Does a scroll target exist?
+  		if (target.length) {
+  			// Only prevent default if animation is actually gonna happen
+  			event.preventDefault();
+  			$('html, body').animate({
+  				scrollTop: target.offset().top
+  			}, 1000, function() {
+  				// Callback after animation
+  				// Must change focus!
+  				var $target = $(target);
+  				$target.focus();
+  				if ($target.is(":focus")) { // Checking if the target was focused
+  					return false;
+  				} else {
+  					$target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+  					$target.focus(); // Set focus again
+  				};
+  			});
+  		}
+  	}
+  });
+}
 
 
 
